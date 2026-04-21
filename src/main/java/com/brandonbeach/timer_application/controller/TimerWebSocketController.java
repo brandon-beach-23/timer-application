209aeff35ae38c21d8a4e728b40edcfc524d59f4
Brandon Beach <brandon.beach23@gmail.com>
2026-04-20 22:51:17 -0400

    Setup TimerWebSocketController and WebSocketConfig.

--- src/main/java/com/brandonbeach/timer_application/controller/TimerWebSocketController.java
+++ src/main/java/com/brandonbeach/timer_application/controller/TimerWebSocketController.java
@@ -1,4 +1,17 @@
 package com.brandonbeach.timer_application.controller;
 
+import com.brandonbeach.timer_application.model.Greeting;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TimerWebSocketController {
+
+    @MessageMapping("/test")
+    @SendTo("/topic/test")
+    public Greeting greeting (String message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting ("Hello, " + message);
+    }
 }
